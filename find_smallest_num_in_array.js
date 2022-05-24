4bf2e3a01d86cbf72f8a8805e181256d24132d5b
nate-peach <nate-peach@github.com>
2022-05-23 17:59:23 -0700

    committing new solution

---
+++ find_smallest_num_in_array.js
@@ -0,0 +1,19 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+// !! So, our function will only be taking in an array? And the array will never be empty and will only ever have integers?
+// !! And we want to be returning a number? Could that number be printed to console or just returned?
+// !! For example, say I call this function returnsSmallestInteger([45, -3, 0, 6, 1]), I would want to see -3 returned?
+// !! So first 1. The program will need to search the array 2. Determine how large a number is based on other numbers in the array. 3. Return the smallest number
+
+function returnsSmallestInteger(intArray) {
+   intArray.sort(function(a, b) {
+       return a - b } );
+       return args[0];
+   
+}
