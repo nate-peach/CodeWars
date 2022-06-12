c081ab10411f1ba0870ee796004a28065db94b62
unknown <nathanpeach90@gmail.com>
2022-06-11 19:18:39 -0700

    adding traffic light solution

---
+++ traffic_light.js
@@ -0,0 +1,18 @@
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// We'll be taking in a single string. Does case matter? Spelling will always be correct?
+// We'll be returning a string. Can we console log the string?
+// For example, if we were taking in the string 'red' we'd expect our output to be 'green'.
+// 1. We need to set up a logical set of instructions 2. We need to return a string
+
+const changeTrafficLightColor = currentColor =>
+    if (currentColor == 'red') {
+        return 'yellow';
+    }
+    if (currentColor == 'yellow') {
+        return 'green';
+    }
+    else {
+        return 'red';
+    }
