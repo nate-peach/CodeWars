9bd557d9c0544ef184a1703d44e07a5493d1f96a
Nate <nathanpeach90@gmail.com>
2022-06-23 18:38:58 -0700

    ADDING SOLUTION

---
+++ highest_lowest.js
@@ -0,0 +1,12 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Notes
+
+//     All numbers are valid Int32, no need to validate them.
+//     There will always be at least one number in the input string.
+//     Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+    numbers =  numbers.split(" ").map(a=>parseInt(a));
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+  }
