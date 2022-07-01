d751c554640a444e39733a35c8969692eda87831
Nate <nathanpeach90@gmail.com>
2022-06-30 19:58:46 -0700

    adding convert boolean solution

---
+++ convert_boolean.js
@@ -0,0 +1,11 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// We're taking in a boolean.
+// We are returning a string. 
+// example
+// we need a conditional
+
+const boolToWord = bool => 
+    bool == true ?
+     'Yes' :
+      'No';
