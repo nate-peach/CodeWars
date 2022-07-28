817b30d66caf4d028bcfc078ade16f0d5513da7a
nate-peach <nathanpeach90@gmail.com>
2022-07-27 21:11:07 -0700

    added PREP comments

---
+++ sum_of_nth_term.js
@@ -0,0 +1,9 @@
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+// We're taking in an integer. It correlates to a specific number in a series. If we input 0, return 0.00
+// We're outputting a natual number, to two decimal places. Can we print to the console? 
+// Ex. input of 5 returns 1.57. The series is 1, 1/4, 1/7, 1/10, 1/13...
+// 1. We need to determine a formula for the series 2. We need to set up conditionals
+
