602e6d15f754d60dae1063d95c6055cc64fb9f81
nate-peach <nathanpeach90@gmail.com>
2022-07-29 09:41:59 -0700

    adding no fives solution

--- no_fives.js
+++ no_fives.js
@@ -4,3 +4,13 @@
 // We're returning all integers between the two inputs, except for any numbers with 5. Can we print to the console?
 // Ex. 1, 10 would return 1, 2, 3, 4, 6, 7, 8, 9, 10
 // 1. We need to return many numbers 2. We need to exclude all 5s
+
+function dontGiveMeFive(start, end) {
+    let numCount = 0;
+    for (let i = start; i <= end; i++) {
+        if (`${i}`.split('').includes('5') == false)  {
+          numCount++;
+        }
+    }
+    return numCount;
+  }
