cebda4a3d1410ab6052ef2610681520d990d55f8
nate-peach <nathanpeach90@gmail.com>
2023-01-12 21:37:59 -0800

    added roman to integer solution

--- roman_to_integer.js
+++ roman_to_integer.js
@@ -10,9 +10,6 @@ might be an OOP option?
 
 const romanToInt = s => {
     let int = 0;
-    let IVandIX = 0;
-    let XLandXC = 0;
-    let CDandCM = 0;
 
     for (let i = 0; i <= s.length; i++) {
         switch (s[i]) {
@@ -24,13 +21,16 @@ const romanToInt = s => {
             case 'D' : int += 500; break
             case 'M' : int += 1000; break
         }
-        if (s[i] == "I" && s[i + 1] == ("V", "X"))
-            IVandIX += 2;
-        if (s[i] == "X" && s[i + 1] == ("L", "C"))
-            XLandXC += 20;
-        if (s[i] == "C" && s[i + 1] == ("D", "M"))
-            CDandCM += 200;
+        if (s[i] == "I" && s[i + 1] == "V" || 
+            s[i] == "I" && s[i + 1] == "M")
+            int -= 2;
+        if (s[i] == "X" && s[i + 1] == "L" || 
+            s[i] == "X" && s[i + 1] == "C")
+            int -= 20;
+        if (s[i] == "C" && s[i + 1] == "D"|| 
+            s[i] == "C" && s[i + 1] == "M")
+            int -= 200;
     }
 
-    return int - IVandIX - XLandXC - CDandCM;
+    return int;
 };
